3991983a75b327f558d46276f799d44e9803d62a
Aliaksei Rak <kventinel@gmail.com>
2022-10-20 21:29:51 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+sudo apt update
+sudo apt full-upgrade
+sudo apt install zsh
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir $HOME/.ssh
