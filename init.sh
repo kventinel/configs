df8117a6151d1c02bbc29146a630ac1e2127b525
Aliaksei Rak <kventinel@gmail.com>
2022-10-20 21:33:41 +0300

    add python3-pip installation

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install -y zsh
+sudo apt install -y \
+    zsh \
+    python3-pip
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir $HOME/.ssh
