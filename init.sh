62adbf5ffc2ae03944b8dbe4f752de146f769eab
Aliaksei Rak <kventinel@gmail.com>
2022-10-20 21:31:09 +0300

    force yes to apt

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt full-upgrade
-sudo apt install zsh
+sudo apt full-upgrade -y
+sudo apt install -y zsh
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir $HOME/.ssh
