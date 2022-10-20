ee54cb0ca53be1714439bd957a2f7142f521d820
Aliaksei Rak <kventinel@gmail.com>
2022-10-20 21:39:09 +0300

    add git lfs

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt install -y \
     zsh \
-    python3-pip
+    python3-pip \
+    git-lfs
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir $HOME/.ssh
