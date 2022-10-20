efda776850213fe14b0531bcccf0a7ea94dd9962
Aliaksei Rak <kventinel@gmail.com>
2022-10-20 21:27:58 +0300

    add init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir $HOME/.ssh
+cp tmux.conf $HOME/.tmux.conf
+cp zprofile $HOME/.zprofile
+cp vimrc-full $HOME/.vimrc
+cp ssh/rc $HOME/.ssh/rc
